c3e4ffd96f0f42a02a6a7910970627f301da3043
amarHdev <henni.amar24@gmail.com>
2021-04-10 17:26:24 +0200

    Ajout des entête des fonctions et structures liées aux la résolution avec Backtracking

--- header.h
+++ header.h
@@ -5,11 +5,10 @@ double fractionalKnapsack(int capacity, struct Item sac[], int n);
 // Structure d'un article qui stocke le poids et la valeur correspondante de l'article
 struct Item {
     int value;
-    int weight;
+    double weight;
 
     // Constructor
-    Item(int value, int weight)
-    {
+    Item(int value, double weight){
        this->value=value;
        this->weight=weight;
     }
@@ -20,3 +19,23 @@ int DPKnapsack(int capacity, int valeurs[], int poids[], int n);
 
 //3- Hearder problème Sac à dos Dynamic programmation (Amelioré)
 int DPKnapsackAmeliorer(int capacity, int valeurs[], int poids[], int n);
+
+
+//4- Backtracking
+struct Item2
+{
+    float weight;
+    int value;
+};
+
+//Structure afin de stocker les informations de l'arbre
+struct Noeud
+{
+    int niveau; //Niveau du nœud dans l'arbre (index dans sac[])
+    int profi; //Profit des nœuds sur le chemin de la racine à ce nœud
+    int bound; //Limite supérieure du profit maximum dans le sous-arbre du nœud
+    float weight;
+};
+bool cmp2(struct Item2 a, struct Item2 b);
+int bound(Noeud u, int n, int capacity, Item2 sac[]);
+int Backtracking(int capacity, Item2 sac[], int n);
